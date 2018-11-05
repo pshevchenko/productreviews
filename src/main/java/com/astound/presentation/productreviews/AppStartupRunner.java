65771116286bc140ac31b3e57982088326c266cb
p.shevchenko <p.shevchenko@astoundcommerce.com>
2018-11-05 16:44:29 +0200

    one more category

--- src/main/java/com/astound/presentation/productreviews/AppStartupRunner.java
+++ src/main/java/com/astound/presentation/productreviews/AppStartupRunner.java
@@ -17,6 +17,7 @@ public class AppStartupRunner implements ApplicationRunner
 	@Override
 	public void run(ApplicationArguments args)
 	{
+		//Category 1
 		Category category1 = Category.builder().id(1).name("Category 1").description("desc").build();
 
 		Product product1 = Product.builder().id(1).name("Product 1").description("desc").build();
@@ -30,5 +31,18 @@ public class AppStartupRunner implements ApplicationRunner
 		category1.setProducts(Arrays.asList(product1, product2, product3));
 
 		CategoryRepository.getCategories().add(category1);
+
+		//Category 2
+		Category category2 = Category.builder().id(2).name("Category 2").description("desc").build();
+
+		Product product4 = Product.builder().id(4).name("Product 4").description("desc").build();
+		Product product5 = Product.builder().id(5).name("Product 5").description("desc").build();
+
+		product4.setReviews(new ArrayList<>());
+		product5.setReviews(new ArrayList<>());
+
+		category2.setProducts(Arrays.asList(product4, product5));
+
+		CategoryRepository.getCategories().add(category2);
 	}
 }
