0cfbfde7d8f5dae8af50e0c34552dd0acf693215
p.shevchenko <p.shevchenko@astoundcommerce.com>
2018-11-07 10:24:04 +0200

    lombok fix

--- src/main/java/com/astound/presentation/productreviews/entities/Product.java
+++ src/main/java/com/astound/presentation/productreviews/entities/Product.java
@@ -1,14 +1,18 @@
 package com.astound.presentation.productreviews.entities;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product
 {
 	private Integer id;
