9bf2961b6022e86fa0df1d253f5f18c49d40afc2
p.shevchenko <p.shevchenko@astoundcommerce.com>
2019-11-19 15:06:51 +0200

    simplify customers

--- src/main/java/com/astound/presentation/productreviews/controllers/HomepageController.java
+++ src/main/java/com/astound/presentation/productreviews/controllers/HomepageController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -39,6 +40,7 @@ public class HomepageController
 	}
 
 	@RequestMapping(value = "/thanks")
+	@ResponseBody
 	public String getThanks()
 	{
 		return "thanks";
